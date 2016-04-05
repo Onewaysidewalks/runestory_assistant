78354715ddebbfd947e090be4c24afddf4af2c22
Samuel Raghunath <onewaysidewalks@gmail.com>
2016-04-05 03:31:43 -0400

    added forum link

--- website/static/app.js
+++ website/static/app.js
@@ -18,6 +18,9 @@ angular.module('app').controller('MainCtrl', function ($scope, $http, $uibModal,
     },
     {
       title: 'Competitive'
+    },
+    {
+      title: 'Runestory Forums'
     }
   ]
 
